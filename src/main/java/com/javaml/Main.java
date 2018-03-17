c16f81419601e762934e7d1b62101dabae2febac
Aleksei Samburov <aesamburov@gmail.com>
2018-03-17 17:26:29 +0300

    Pipeline for expression parsing by image

--- src/main/java/com/javaml/Main.java
+++ src/main/java/com/javaml/Main.java
@@ -1,7 +1,5 @@
 package com.javaml;
 
-import com.javaml.concurrent.CheckedFunction;
-import com.javaml.concurrent.ThreadPool;
 import com.javaml.converting.converter.ImageConverter;
 import com.javaml.converting.converter.SimpleImageConverter;
 import com.javaml.image.AsciiImage;
@@ -9,7 +7,6 @@ import com.javaml.ml.Tensor;
 import com.javaml.ml.classifier.LogisticNaryClassifier;
 import com.javaml.ml.classifier.NaryClassifier;
 import com.javaml.segmentation.segmenter.ExpressionSegmenter;
-import com.javaml.segmentation.segmenter.LinearSegmenter;
 import com.javaml.segmentation.segmenter.Segmenter;
 
 import static spark.Spark.*;
@@ -24,6 +21,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Main {
     public static void main(String[] args) {
@@ -52,6 +50,20 @@ class Main {
         });
     }
 
+    private static NaryClassifier classifier = train("/PATH/TO/TRAIN");
+    private static Map<Integer, String> labelMapping = new HashMap<>();
+    static {
+        for (int i = 0; i < 10; i++) {
+            labelMapping.put(i, String.valueOf(i));
+        }
+        labelMapping.put(10, "(");
+        labelMapping.put(11, ")");
+        labelMapping.put(12, "+");
+        labelMapping.put(13, "-");
+        labelMapping.put(14, "*");
+        labelMapping.put(15, "/");
+    }
+
     private static void convertImage(String path) throws Exception {
         BufferedImage image = ImageIO.read(new File(path));
         ImageConverter converter = new SimpleImageConverter(image.getHeight(), image.getWidth());
@@ -71,68 +83,50 @@ class Main {
         System.out.println(scaledImage);
     }
 
-    private static void segmentImage(String path) throws Exception {
-        BufferedImage image = ImageIO.read(new File(path));
+    private static String parseExp(String pathToFile) throws Exception {
+        BufferedImage image = ImageIO.read(new File(pathToFile));
         ImageConverter converter = new SimpleImageConverter(image.getHeight(), image.getWidth());
 
         AsciiImage asciiImage = converter.convert(image);
-        Segmenter segmenter = new ExpressionSegmenter();//LinearSegmenter(LinearSegmenter.BackgroundFetchStrategy.FIRST);
-        List<AsciiImage> images = segmenter.segment(asciiImage);
+        List<AsciiImage> segments = segmentImage(asciiImage);
 
-        for(int i = 0; i < images.size(); i++) {
-            System.out.println("Image №" + (i+1) + ": ");
-            System.out.println(images.get(i));
-        }
+        List<String> labels = predictLabels(classifier, segments);
+        return String.join("", labels);
     }
 
-    private static void predictNumber(String pathToTrain, String pathToImage) throws Exception{
-        List<Tensor<Number>> train = new ArrayList<>();
-        List<Integer> labels = new ArrayList<>();
-
-        NaryClassifier c = new LogisticNaryClassifier(100, 0.7, 10);
-        System.out.println(String.format("Training"));
-
-        c.fit(train, labels);
-
-        System.out.println("Complete!");
-
-        BufferedImage image = ImageIO.read(new File(pathToImage));
-        ImageConverter converter = new SimpleImageConverter(image.getHeight(), image.getWidth());
-        AsciiImage asciiImage = converter.convert(image);
-
-        Segmenter segmenter = new LinearSegmenter(LinearSegmenter.BackgroundFetchStrategy.FIRST);
-        List<AsciiImage> images = segmenter.segment(asciiImage);
-
-        CheckedFunction<AsciiImage, Integer> predictOneImage = (AsciiImage img) ->
-                c.predict(img.getScaled(28, 28));
-
-        ThreadPool threadPool = new ThreadPool();
-        Collection<Integer> answers = threadPool.parallelMap(predictOneImage, images);
-
-        System.out.println("Numbers: " + answers);
+    private static List<AsciiImage> segmentImage(AsciiImage image) throws Exception {
+        Segmenter segmenter = new ExpressionSegmenter();
+        return segmenter.segment(image);
     }
 
-    private static NaryClassifier train(String pathToTrain) throws Exception {
+    private static List<String> predictLabels(NaryClassifier classifier, List<AsciiImage> images) throws Exception {
+        try {
+            return classifier.predict(images)
+                    .stream()
+                    .map((x) -> labelMapping.get(x))
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        }
+        return null;
+    }
+
+    private static NaryClassifier train(String pathToTrain) {
         List<Tensor<Number>> train = new ArrayList<>();
         List<Integer> labels = new ArrayList<>();
-        Map<Integer, String> labelMapping = new HashMap<>();
-        for (int i = 0; i < 10; i++) {
-            labelMapping.put(i, String.valueOf(i));
-        }
-        labelMapping.put(10, "(");
-        labelMapping.put(11, ")");
-        labelMapping.put(12, "+");
-        labelMapping.put(13, "-");
-        labelMapping.put(14, "*");
-        labelMapping.put(15, "/");
 
         for (int labelId : labelMapping.keySet()) {
-
             Path pathToClass = Paths.get(pathToTrain, String.valueOf(labelId));
             File folder = new File(pathToClass.toString());
 
             for (final File file : folder.listFiles()) {
-                BufferedImage image = ImageIO.read(file);
+                BufferedImage image = null;
+                try {
+                    image = ImageIO.read(file);
+                } catch (Exception e) {
+
+                }
+
                 ImageConverter converter = new SimpleImageConverter(28, 28);
 
                 AsciiImage asciiImage = converter.convert(image);
@@ -142,9 +136,14 @@ class Main {
         }
 
         long start_time = System.nanoTime();
-        System.out.println(String.format("Training"));
+        System.out.println("Training");
         NaryClassifier c = new LogisticNaryClassifier(100, 0.7, 10);
-        c.fit(train, labels);
+
+        try {
+            c.fit(train, labels);
+        } catch (Exception e) {
+
+        }
 
         long end_time = System.nanoTime();
         double difference = (end_time - start_time) / 1e6;
